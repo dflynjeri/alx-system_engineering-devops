d10e15a0d51c4694df3923a81ddbe05c5e58d367
dflynjeri <dflynjeri@gmail.com>
2024-08-17 08:35:22 +0300

    My Configuration management project

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# 0-create_a_file.pp
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,9 @@
+# 2-execute_a_command.pp
+# This Puppet manifest kills a process named 'killmenow' using pkill
+
+exec { 'kill_killmenow':
+  command  => '/usr/bin/pkill killmenow',
+  provider  => 'shell',
+  returns  =>  [0, 1],
+}
+
